7b15197385abb1c7071f6e9aa59f35e8bc3b54c1
Jeevan S <hrgovernancesolutions@gmail.com>
2025-11-26 09:42:17 +0530

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 // Component Imports
 import Navbar from "./components/navbar/Navbar";
