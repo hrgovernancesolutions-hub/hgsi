d41569658c1fceace50862f700ffc36af6264ceb
Jeevan S <hrgovernancesolutions@gmail.com>
2026-01-29 17:13:52 +0530

    teamimages

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -13,10 +13,13 @@ import img8 from "../../assets/img8.jpg";
 import img9 from "../../assets/img9.jpg";
 import img10 from "../../assets/img10.jpg";
 import img11 from "../../assets/img11.jpg";
+import img12 from "../../assets/crst1.jpg";
+import img13 from "../../assets/crst2.jpg";
+import img14 from "../../assets/crst3.jpg";
 
 export default function Gallery() {
   const images = [
-    img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11
+    img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14
   ];
 
   const [popupIndex, setPopupIndex] = useState(null);
